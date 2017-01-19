95363546dc3cfa7b156e852ad4d2e930d46a4fcb
Blue <Blue@bluedemacbook-pro.local>
2017-01-19 16:13:52 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by  Blue on 2017/1/19.
+//  Copyright © 2017年  Blue. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
